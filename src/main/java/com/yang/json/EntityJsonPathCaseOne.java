146ef7ec7f7e2661bd5b41c6e6f69fa90c63459d
baoYang <1411091515@qq.com>
2019-11-05 22:53:06 +0800

    feat JSONPath的使用提交代码

--- src/main/java/com/yang/json/EntityJsonPathCaseOne.java
+++ src/main/java/com/yang/json/EntityJsonPathCaseOne.java
@@ -3,6 +3,8 @@ package com.yang.json;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONPath;
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assert;
+import org.springframework.util.CollectionUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,7 +21,9 @@ public class EntityJsonPathCaseOne {
 
     public static void main(String[] args) {
         // entityOneSimple();
-        getManyParams();
+        // getManyParams();
+        // returnMany();
+        returnRange();
     }
 
     private static void entityOneSimple(){
@@ -47,6 +51,33 @@ public class EntityJsonPathCaseOne {
         log.info("  jsonArray ---> {} " , jsonArray);
     }
 
+    public static void returnMany(){
+        List<Entity> entities = new ArrayList<Entity>();
+        entities.add(new Entity("Gavin"));
+        entities.add(new Entity("Peter"));
+        entities.add(new Entity("Zero"));
+
+        List<Entity> result =(List<Entity>) JSONPath.eval(entities,"[1,2]");
+        if(!CollectionUtils.isEmpty(result) && result.size() == 2){
+            System.out.println(result.get(0).getName());
+            System.out.println(result.get(1).getName());
+        }
+    }
+
+    public static void returnRange(){
+        List<Entity> entities = new ArrayList<Entity>();
+        entities.add(new Entity("Gavin"));
+        entities.add(new Entity("Peter"));
+        entities.add(new Entity("Zero"));
+
+        List<Entity> result =(List<Entity>) JSONPath.eval(entities,"[0:2]");
+        if(!CollectionUtils.isEmpty(result) && result.size() == 3){
+            System.out.println(result.get(0).getName());
+            System.out.println(result.get(1).getName());
+            System.out.println(result.get(2).getName());
+        }
+
+    }
 
 }
 
