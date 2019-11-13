00ecc3eeb9b045a8d48aa143141cae0ecc81da05
木易 <1411091515@qq.com>
2019-11-13 21:29:57 +0800

    feat 提交Basic基础类

---
+++ src/main/java/com/yang/netty/basic/BasicMesg.java
@@ -0,0 +1,19 @@
+package com.yang.netty.basic;
+
+/***********************************************************************
+ *<PRE>
+ *
+ *  File Name       : 
+ *
+ *  Creation Date   : 19-11-13
+ *
+ *  Author          : Gavin
+ *
+ *  Purpose         : 
+ *
+ *  History         : 
+ *
+ *</PRE>
+ ***************************************************************************/
+public class BasicMesg {
+}
