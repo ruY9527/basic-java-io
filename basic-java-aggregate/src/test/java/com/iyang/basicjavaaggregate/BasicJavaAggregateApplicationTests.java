63b06c0901e37c699708b19605722a764d39a782
木易 <1411091515@qq.com>
2019-12-08 16:12:10 +0800

    feat 提交集合模块初始化代码

---
+++ basic-java-aggregate/src/test/java/com/iyang/basicjavaaggregate/BasicJavaAggregateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iyang.basicjavaaggregate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicJavaAggregateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
